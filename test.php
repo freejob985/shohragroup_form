76d9e32971625cabce64f3590c24eae2c9ce8b33
freejob985 <freejob985@gmail.com>
2021-03-28 18:06:58 +0200

    asd

--- test.php
+++ test.php
@@ -934,6 +934,97 @@ body,h1,h2,h3,h4,h5,h6,p,a,input,span,label{   font-family: 'Tajawal', sans-seri
                      <div class="col-sm-6"></div>
                   </div>
              
+
+                  <div class="row">
+   <div class="form-group">
+      <p class="bold col-sm-12">
+         <strong>إختر الفئات اللتي تختص فيها: <span class="text-danger">*</span></strong>
+      </p>
+      <div class="col-sm-12">
+         <ul class="list-unstyled row">
+            <li class="col-sm-3">
+               <input id="SelCategories" name="SelCategories" type="checkbox" value="1"><input name="SelCategories" type="hidden" value="false">
+               الإدارية
+               <ul class="list-unstyled" style="padding: 5px 20px;">
+                  <li> <input id="SelCategories" name="SelCategories" type="checkbox" value="10"><input name="SelCategories" type="hidden" value="false">
+                     إدارة الأعمال
+                  </li>
+                  <li> <input id="SelCategories" name="SelCategories" type="checkbox" value="11"><input name="SelCategories" type="hidden" value="false">
+                     التطوير الشخصي ومهارات الاتصال
+                  </li>
+                  <li> <input id="SelCategories" name="SelCategories" type="checkbox" value="12"><input name="SelCategories" type="hidden" value="false">
+                     إدارة المشاريع والعقود والإدارة الهندسية
+                  </li>
+                  <li> <input id="SelCategories" name="SelCategories" type="checkbox" value="13"><input name="SelCategories" type="hidden" value="false">
+                     المالية والمحاسبية والتكاليف
+                  </li>
+                  <li> <input id="SelCategories" name="SelCategories" type="checkbox" value="14"><input name="SelCategories" type="hidden" value="false">
+                     الموارد البشرية
+                  </li>
+                  <li> <input id="SelCategories" name="SelCategories" type="checkbox" value="15"><input name="SelCategories" type="hidden" value="false">
+                     إدارة المواد التوريد والمشتريات
+                  </li>
+                  <li> <input id="SelCategories" name="SelCategories" type="checkbox" value="16"><input name="SelCategories" type="hidden" value="false">
+                     السكرتارية وإدارة المكاتب
+                  </li>
+                  <li> <input id="SelCategories" name="SelCategories" type="checkbox" value="17"><input name="SelCategories" type="hidden" value="false">
+                     العلاقات العامة وخدمات العملاء
+                  </li>
+                  <li> <input id="SelCategories" name="SelCategories" type="checkbox" value="18"><input name="SelCategories" type="hidden" value="false">
+                     التسويق والمبيعات
+                  </li>
+               </ul>
+            </li>
+            <li class="col-sm-3">
+               <input id="SelCategories" name="SelCategories" type="checkbox" value="2"><input name="SelCategories" type="hidden" value="false">
+               الأمن، الصحة والسلامة
+               <ul class="list-unstyled" style="padding: 5px 20px;">
+                  <li> <input id="SelCategories" name="SelCategories" type="checkbox" value="19"><input name="SelCategories" type="hidden" value="false">
+                     الصحة والسلامة
+                  </li>
+                  <li> <input id="SelCategories" name="SelCategories" type="checkbox" value="20"><input name="SelCategories" type="hidden" value="false">
+                     الأمن
+                  </li>
+               </ul>
+            </li>
+            <li class="col-sm-3">
+               <input id="SelCategories" name="SelCategories" type="checkbox" value="3"><input name="SelCategories" type="hidden" value="false">
+               الفنية
+               <ul class="list-unstyled" style="padding: 5px 20px;">
+                  <li> <input id="SelCategories" name="SelCategories" type="checkbox" value="4"><input name="SelCategories" type="hidden" value="false">
+                     الهندسة الكهربائية
+                  </li>
+                  <li> <input id="SelCategories" name="SelCategories" type="checkbox" value="5"><input name="SelCategories" type="hidden" value="false">
+                     الهندسة العامة
+                  </li>
+                  <li> <input id="SelCategories" name="SelCategories" type="checkbox" value="6"><input name="SelCategories" type="hidden" value="false">
+                     الهندسة الميكانيكية والمرافق الصناعية
+                  </li>
+                  <li> <input id="SelCategories" name="SelCategories" type="checkbox" value="7"><input name="SelCategories" type="hidden" value="false">
+                     هندسة البترول، الغاز، الكيمياويات والمعالجات
+                  </li>
+                  <li> <input id="SelCategories" name="SelCategories" type="checkbox" value="8"><input name="SelCategories" type="hidden" value="false">
+                     هندسة التوازن، الاستقرار، الصيانة والفحص
+                  </li>
+                  <li> <input id="SelCategories" name="SelCategories" type="checkbox" value="9"><input name="SelCategories" type="hidden" value="false">
+                     هندسة المعادن واللحام والتآكل
+                  </li>
+                  <li> <input id="SelCategories" name="SelCategories" type="checkbox" value="21"><input name="SelCategories" type="hidden" value="false">
+                     اللوجستية
+                  </li>
+               </ul>
+            </li>
+            <li class="col-sm-3">
+               <input id="OtherCategories" name="chkOther" type="checkbox" value="true"> آخر
+               <div id="divOther" class="mt-15" style="display: none">
+                  <textarea class="form-control" cols="20" id="OtherCategories" name="OtherCategories" rows="2"></textarea>
+               </div>
+            </li>
+         </ul>
+      </div>
+   </div>
+</div>
+
                </div>
              
                <div id="captcha-wraper" class="form-group">
